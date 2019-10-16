86f9d964c2177a124aa0d70b07a34a8c7704b48b
Lucas <lucascruzati@gmail.com>
2019-10-15 21:14:00 -0300

    removing unnecessary file

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -3,10 +3,7 @@ import { combineReducers } from 'redux';
 import auth from './auth/reducer';
 import user from './user/reducer';
 
-import meetup from './meetups/reducer';
-
 export default combineReducers({
   auth,
   user,
-  meetup,
 });
